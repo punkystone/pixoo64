e6066c230860b2d70dea51fafda44599e9edb9bd
Kevin Leopold <leopold@it-intouch.de>
2022-12-24 16:47:39 +0100

    add display to set_error

--- src/error/set_error.rs
+++ src/error/set_error.rs
@@ -1,6 +1,17 @@
+use std::fmt::Display;
+
 use super::{set_access_error::SetAccessError, set_out_of_range_error::SetOutOfRangeError};
 
 pub enum SetError {
     SetOutOfRangeError(SetOutOfRangeError),
     SetAccessError(SetAccessError),
+}
+
+impl Display for SetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SetError::SetOutOfRangeError(error) => write!(f, "{error}"),
+            SetError::SetAccessError(error) => write!(f, "{error}"),
+        }
+    }
 }

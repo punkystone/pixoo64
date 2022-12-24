eeecbc7e435ade33e2da17ba5c04d933b3388c39
Kevin Leopold <leopold@it-intouch.de>
2022-12-24 16:54:00 +0100

    fix max value

--- src/error/set_out_of_range_error.rs
+++ src/error/set_out_of_range_error.rs
@@ -13,10 +13,20 @@ impl Display for SetOutOfRangeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.input {
             SetOutOfRangeErrorInput::X(value) => {
-                write!(f, "Invalid X value: {} (Min: 0, Max: {})", value, SIZE_X)
+                write!(
+                    f,
+                    "Invalid X value: {} (Min: 0, Max: {})",
+                    value,
+                    SIZE_X - 1
+                )
             }
             SetOutOfRangeErrorInput::Y(value) => {
-                write!(f, "Invalid Y value: {} (Min: 0, Max: {})", value, SIZE_Y)
+                write!(
+                    f,
+                    "Invalid Y value: {} (Min: 0, Max: {})",
+                    value,
+                    SIZE_Y - 1
+                )
             }
         }
     }
